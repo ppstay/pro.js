7aaecf0d88423270347cdd0b00d5613da997a705
Andy Chen <ppstay@hotmail.com>
2017-12-18 20:19:16 +0800

    init

---
+++ misc/ch2-01.js
@@ -0,0 +1,19 @@
+/* 
+ * This is a multi-line
+ * Comment
+ */
+
+ // Single line comment
+
+ "use strict";
+
+ function doSomething(){
+ 	"use strict";
+ 	// function body;
+ }
+
+var a = 1;
+var b = 2;
+var sum = a + b;
+
+typeof(a);
